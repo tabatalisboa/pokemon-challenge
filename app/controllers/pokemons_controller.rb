a5390c6b6c435175791bf1d986987c2a3432f094
tabatalisboa <tabatalisboa@pm.me>
2019-11-10 20:19:15 -0300

    Update d. GitHub security vulnerability issue

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,18 +1,11 @@
 require 'json'
 require 'open-uri'
 
-
 class PokemonsController < ApplicationController
-
+  before_action :find_pokemon
   def show
     @pokemon = Pokemon.find(params[:id])
-
-    url = "https://pokeapi.co/api/v2/pokemon/#{params[:id]}"
-    pokemon_serialized = open(url).read
-    pokemon_features = JSON.parse(pokemon_serialized)
-
-    @pokemon = Pokemon.new(name: pokemon_features[:id], pokemon_type: pokemon_features["types"][0]["type"]["name"])
-
+    @pokemon = Pokemon.new(name: pokemon_features[:name], pokemon_type: pokemon_features["types"][0]["type"]["name"])
   end
 
   def new
@@ -21,17 +14,22 @@ class PokemonsController < ApplicationController
 
   def create
     @pokemon = Pokemon.new(pokemon_params)
-    redirect_to pokemon_url(@pokemon(params[:id]))
-    # if @pokemon.save
-    #   # redirect_to teams_root
-    # else
-    #   # redirect_to new_pokemon_path
-    # end
+    if @pokemon.save
+      redirect_to :show
+    else
+      redirect_to new_pokemon_path
+    end
   end
 
   private
 
   def pokemon_params
     params.require(:pokemon).permit(:name, :pokemon_type)
+  end
+
+  def find_pokemon
+    url = "https://pokeapi.co/api/v2/pokemon/#{params[:id]}"
+    pokemon_serialized = open(url).read
+    pokemon_features = JSON.parse(pokemon_serialized)
   end
 end
